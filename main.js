1f477b5d2fb9dbdf218916c4ef7f770d041e7ad1
hussein <118814567+husseinomar19@users.noreply.github.com>
2023-05-10 16:10:43 +0200

    click even dark white mood.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const webcom = document.createElement('webcom');
 webcom.innerHTML = `
-<header>
+<header id="hed">
         <h2 id="tit">Web-Com.nl</h2>
         <ul>
             <li><a href="">Home</a></li>
@@ -8,12 +8,13 @@ webcom.innerHTML = `
             <li><a href="">Contact</a></li>
         </ul>
         <button>Inloggen</button>
+        <button id="sw">Dark mood</button>
     </header>
 `
 const titwebcom = document.createElement('titwebcom');
 titwebcom.innerHTML = `
 <div class="tit-webcom">
-       <h2>Hallo this is my custom component</h2>
+       <h2 id="titcolor">Hallo this is my custom component</h2>
    </div>
 `
 
@@ -32,5 +33,26 @@ class tithussein extends HTMLElement{
 }
 
 
+
 customElements.define('nav-hussein',husseinwebcom);
 customElements.define('tit-hussein',tithussein)
+
+
+    let sw = document.getElementById('sw');
+    let hed = document.getElementById('hed');
+    let titcolorList = document.querySelectorAll('#titcolor');
+
+sw.addEventListener('click', function(){
+    const currentColor = window.getComputedStyle(document.body).getPropertyValue('background-color');
+    if (currentColor === 'rgb(255, 255, 255)') {
+        document.body.style.backgroundColor = 'black';
+        sw.innerHTML = "Dark mood"
+        titcolorList.forEach(titcolor => titcolor.style.color = "white");
+      } else {
+        document.body.style.backgroundColor = 'white';
+        sw.innerHTML = "white mood" 
+        titcolorList.forEach(titcolor => titcolor.style.color = "black");
+      }
+    
+})
+
